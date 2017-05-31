96414c0f8e344fdcf96ada1e850908abacc243de
James O'Shea <jamesoshea89@hotmail.com>
2017-05-31 16:02:10 +0200

    summaries feature added

--- src/Home.vue
+++ src/Home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <h4 v-if="this.todos">Here are some things to do:</h4>
+    <h4 v-if="this.todos.length > 0">Your todos:</h4>
     <div class="todo" v-for="(todo, i) in todos">
       <h5>{{ todo.title }}</h5>
-      <p>{{ todo.text }} <br>
-      submitted at: {{ todo.time }}</p>
+      <p @click="showText(i)" class="summary">{{ todo.summary }} </p>
+      <p>submitted at: {{ todo.time }}</p>
       <button @click="deleteTodo(i)">Delete this</button>
       <hr>
     </div>
@@ -30,20 +30,32 @@
         let hours = padZeros(date.getHours())
         let minutes = padZeros(date.getMinutes())
         let time = hours + ':' + minutes
-        this.todos.push({ title: this.newTitle, text: this.newText, time: time})
+        this.todos.push({ title: this.newTitle, text: this.newText, summary: this.newText.slice(0, 50) + '...', time: time, expanded: false})
         this.newTitle = this.newText = ''
       },
       deleteTodo(i) {
         this.todos.splice(i, 1);
+      },
+      showText(i) {
+        if(!this.todos[i].expanded) {
+          this.todos[i].summary = this.todos[i].text
+          this.todos[i].expanded = !this.todos[i].expanded
+        } else {
+          this.todos[i].summary = this.todos[i].text.slice(0, 50) + '...'
+          this.todos[i].expanded = !this.todos[i].expanded
+        }
       }
     }
   }
 
   function padZeros(inp){
-    if (inp < 10) {
-      return Number('0' + inp);
+    if (inp == 0) {
+      return '00'
     }
-    return Number(inp);
+    else if (inp < 10 && > 0) {
+      return '0' + inp;
+    }
+    return String(inp);
   }
 </script>
 
@@ -51,5 +63,10 @@
 .todo {
   width: 80%;
   margin: auto;
+}
+
+.summary {
+  color: #42b983;
+  text-decoration: underline;
 }
 </style>
